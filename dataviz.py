3f37a77ac492716be44257c5164d9da01682bab8
charlyalizadeh <charlypro1502@outlook.fr>
2021-01-02 14:45:30 +0100

    Refactore the codeThe data sets are now directly converted to pandas dataframe.Split the  method into multiple smaller methods.

--- dataviz.py
+++ dataviz.py
@@ -9,51 +9,70 @@ from sklearn.decomposition import PCA
 
 class DataViz:
     def __init__(self, train_set=None, test_set=None):
-        self.train_set = train_set
-        self.test_set = test_set
-        sns.set_theme(style="whitegrid")
+        self.label = {1: 'Red soil',
+                      2: 'Cotton crop',
+                      3: 'Grey soil',
+                      4: 'Damp grey soil',
+                      5: 'Soil with vegetation stubble',
+                      6: 'Mixture class',
+                      7: 'Very damp grey soil'}
+        self.train_set = self.convert_numpy_arr_to_df(train_set)
+        self.test_set = self.convert_numpy_arr_to_df(test_set)
 
-    def import_test_set(self, path_test):
-        self.test_set = np.loadtxt(path_test)
+    def import_train_set_from_txt(self, path_train):
+        self.train_set = self.convert_numpy_arr_to_df(np.loadtxt(path_train))
 
-    def import_train_set(self, path_train):
-        self.train_set = np.loadtxt(path_train)
+    def import_test_set_from_txt(self, path_test):
+        self.test_set = self.convert_numpy_arr_to_df(np.loadtxt(path_test))
 
-    def plot_spectrum(self,
-                      pixel_index=4,
-                      spectrum_index=(0, 1, 2, 3),
-                      train=True,
-                      **kwargs):
-        pixel_index *= 4
-        spectrum_tot = {0: 'green',
-                        1: 'red',
-                        2: 'indra_red1',
-                        3: 'infra_red2'}
-        mat_value = self.train_set if train else self.test_set
-        values = {}
+    def import_train_set(self, train_set):
+        self.train_set = self.convert_numpy_arr_to_df(train_set)
 
-        for index in spectrum_index:
-            unique, counts = np.unique(mat_value[:, pixel_index + index],
-                                       return_counts=True)
-            temp_dict = defaultdict(lambda: 0)
-            temp_dict.update(dict(zip(unique, counts)))
-            values[spectrum_tot[index]] = [temp_dict[i] for i in range(256)]
-        values["index"] = range(256)
-        df = pd.DataFrame.from_dict(values)
+    def import_test_set(self, test_set):
+        self.test_set = self.convert_numpy_arr_to_df(test_set)
+
+    def convert_numpy_arr_to_df(self, np_array):
+        df = pd.DataFrame(
+                data=np_array,
+                columns=[f'p{int(p / 4)}_sp{p % 4}'
+                         for p in range(36)] + ['Class']
+                )
+        df['Class'] = df['Class'].astype(int)
+        df['Class'] = df['Class'].replace(self.label)
+        return df
+
+    def plot_spectrum(self, pixel_index=4, spectrum_index=range(4),
+                      train=True, **kwargs):
+
+        query_df = None
+        if train:
+            query_df = self.train_set[[
+                f'p{pixel_index}_sp{sp}' for sp in spectrum_index
+                ]]
+        else:
+            query_df = self.test_set[[
+                f'p{pixel_index}_sp{sp}' for sp in spectrum_index
+                ]]
+        counts = [
+                query_df[col].astype(int).value_counts()
+                for col in query_df.columns
+                ]
+        for i in range(len(counts)):
+            counts[i] = [counts[i][j] if j in counts[i] else 0
+                         for j in range(256)]
+        df = pd.DataFrame(dict(zip([f'p{pixel_index}_sp{sp}'
+                                    for sp in spectrum_index], counts)))
+        df['Pixel Value'] = range(256)
         palette = ['green', 'red', 'orange', 'navy']
         palette = palette[:len(spectrum_index)]
-        return sns.lineplot(x='index',
+        return sns.lineplot(x='Pixel Value',
                             y='value',
                             hue='variable',
-                            data=pd.melt(df, ['index']),
-                            palette=palette,
+                            data=pd.melt(df, ['Pixel Value']),
                             **kwargs)
 
-    def plot_line_pixel_value(self,
-                              pixel_index=range(8),
-                              spectrum_index=[0, 1, 2, 3],
-                              train=True,
-                              **kwargs):
+    def plot_line_pixel_value(self, pixel_index=range(8),
+                              spectrum_index=range(4), train=True, **kwargs):
         x = []
         y = []
         h = []
@@ -67,50 +86,36 @@ class DataViz:
                 h.append(int(pindex / 4))
         return sns.stripplot(x=x, y=y, hue=h)
 
-    def bar_plot_label(self, train=True):
+    def plot_bar_class(self, train=True):
         """Bar plot the number of observations by class.
 
-        :param bool train: Boolean indicating whether to use the training or testing set. (Default True)
+        :param bool train: Boolean indicating whether to use the training
+                           or testing set. (Default True)
         """
 
-        # label_index is the 37th column in the training and testing set
-        labels = {
-            1: "Red soil",
-            2: "Cotton crop",
-            3: "Grey soil",
-            4: "Damp grey \n soil",
-            5: "Soil with \n vegetation stubble",
-            6: "Mixture\n class",
-            7: "Very damp\n grey soil",
-        }
-        mat_value = self.train_set if train else self.test_set
-        unique, counts = np.unique(mat_value[:, 36], return_counts=True)
-        # Add class 6 as there are no examples with class 6 in our dataset
-        unique = np.insert(unique, 5, 6.0)
-        counts = np.insert(counts, 5, 0)
-        # Changing font of labels
-        hfont = {"fontname": "Microsoft New Tai Lue"}
-        # Plot barcharts
-        fig = plt.figure(figsize=(10, 5))
-        plt.bar(unique, counts, color="blue", width=1)
-        plt.xlabel("Label", **hfont)
-        plt.ylabel("No. of pixels labelled", **hfont)
-        plt.title("Bar Plot of classes")
-        # Change xticks to plot labels names instead of numbers
-        plt.xticks(unique, labels.values(), **hfont)
-        plt.tight_layout()
-        return fig
+        counts = self.train_set['Class'].value_counts() \
+            if train else \
+            self.test_set['Class'].value_counts()
+        for i in self.label.values():
+            if i not in counts.index:
+                counts.at[i] = 0
+        ax = sns.barplot(x=counts.index, y=counts)
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=30)
+        ax.set(xlabel='Label',
+               ylabel='No. of pixels labelled',
+               title='Bar Plot of classes')
+        return ax
 
-    def pca(self, train=True, number_components=5):
-        mat_value = self.train_set if train else self.test_set
+    def get_pca(self, train=True, number_components=2):
+        """
 
-        # Store the value in a DataFrame
-        df = pd.DataFrame(data=mat_value, index=[i for i in range(len(mat_value))])
+        source: https://towardsdatascience.com/pca-using-python-scikit-learn-e653f8989e60
+        """
+        df = self.train_set if train else self.test_set
 
         # Store the features values
         df_features = df.iloc[:, 0:36].values
-        # Store the target values
-        # df_target = df.iloc[:, 36].values
+        # Store the class values
 
         # Scale the features
         df_features = StandardScaler().fit_transform(df_features)
@@ -118,68 +123,70 @@ class DataViz:
         # Apply PCA on features
         pca = PCA(n_components=number_components)
         principal_components = pca.fit_transform(df_features)
+        return pca, principal_components
 
+    def plot_explained_variance(self, pca):
+        ax = plt.subplot()
+        ax.bar(range(1, len(pca.explained_variance_) + 1),
+               pca.explained_variance_)
+        ax.set_ylabel('Explained variance')
+        ax.set_xlabel('Components')
+        ax.set_title('Explained variance Plot')
+        ax.plot(
+            range(1, len(pca.explained_variance_) + 1),
+            np.cumsum(pca.explained_variance_),
+            c='red',
+            label='Cumulative Explained Variance',
+        )
+        plt.legend(loc='upper left')
+        plt.tight_layout()
+        return ax
+
+    def plot_scree(self, pca):
+        ax = plt.subplot()
+        ax.plot(range(1, len(pca.explained_variance_) + 1),
+                pca.explained_variance_)
+        ax.set_xlabel('Number of components')
+        ax.set_ylabel('Cumulative explained variance')
+        plt.tight_layout()
+        return ax
+
+    def plot_pca(self, pca, principal_components, classes):
         # Store the PCA values in a DataFrame
         principal_comp_df = pd.DataFrame(
-            data=principal_components, columns=["pc_1", "pc_2", "pc_3", "pc_4", "pc_5"]
+            data=principal_components,
+            columns=[f'PC{i}' for i in range(1, pca.n_components_ + 1)]
         )
 
         # Add the target values and rename it
-        final_df = pd.concat([principal_comp_df, df.iloc[:, 36]], axis=1)
-        final_df = final_df.rename(columns={36: "target"})
-
-        # Explained variance plot
-        plt.bar(range(1, len(pca.explained_variance_) + 1), pca.explained_variance_)
-        plt.ylabel("Explained variance")
-        plt.xlabel("Components")
-        plt.title("Explained variance Plot")
-        plt.plot(
-            range(1, len(pca.explained_variance_) + 1),
-            np.cumsum(pca.explained_variance_),
-            c="red",
-            label="Cumulative Explained Variance",
-        )
-        plt.legend(loc="upper left")
-        plt.tight_layout()
-        plt.show()
-
-        # Scree plot
-        plt.plot(pca.explained_variance_)
-        plt.xlabel("Number of components")
-        plt.ylabel("Cumulative explained variance")
-        plt.tight_layout()
-        plt.show()
-        # First component is plotted at 0 in xscale
+        final_df = pd.concat([principal_comp_df, classes], axis=1)
 
         # 2D projection
-
         fig = plt.figure(figsize=(8, 8))
         ax = fig.add_subplot(1, 1, 1)
-        ax.set_xlabel("Principal Component 1", fontsize=15)
-        ax.set_ylabel("Principal Component 2", fontsize=15)
-        ax.set_title("PCA with 2 Components", fontsize=20)
+        ax.set_xlabel('Principal Component 1', fontsize=15)
+        ax.set_ylabel('Principal Component 2', fontsize=15)
+        ax.set_title('PCA with 2 Components', fontsize=20)
 
-        targets = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0]
-        # targets= ['Red soil','Cotton crop','Grey soil','Damp grey soil','Soil with vegetation stubble','Mixture class','Very damp grey soil']
+        targets = list(self.label.values())
 
-        # colors for our different labels
+        # Colors for our different labels
         colors = [
-            "crimson",
-            "bisque",
-            "silver",
-            "dimgrey",
-            "limegreen",
-            "royalblue",
-            "blueviolet",
+            'crimson',
+            'bisque',
+            'silver',
+            'dimgrey',
+            'limegreen',
+            'royalblue',
+            'blueviolet',
         ]
 
         # We plot with only the two first components
-
         for target, color in zip(targets, colors):
-            indicesToKeep = final_df["target"] == target
+            indicesToKeep = final_df['Class'] == target
             ax.scatter(
-                final_df.loc[indicesToKeep, "pc_1"],
-                final_df.loc[indicesToKeep, "pc_2"],
+                final_df.loc[indicesToKeep, 'PC1'],
+                final_df.loc[indicesToKeep, 'PC2'],
                 c=color,
                 s=50,
             )
@@ -187,4 +194,4 @@ class DataViz:
         ax.legend(targets)
         ax.grid()
         plt.tight_layout()
-        plt.show()
+        return ax
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from dataviz import DataViz
+import matplotlib.pyplot as plt
 
 
 def main():
-    pass
+    test = DataViz()
+    test.import_train_set_from_txt("./sat.trn")
+    test.import_test_set_from_txt("./sat.tst")
+    pca, principal_components = test.get_pca(True, 6)
+    test.plot_scree(pca)
+    plt.show()
+    test.plot_explained_variance(pca)
+    plt.show()
+    test.plot_pca(pca, principal_components, test.train_set.iloc[:, 36])
+    plt.show()
 
 
 if __name__ == '__main__':
