d215ac62db896b5bc160677d2d77d9e05a1ae9e5
charlyalizadeh <charlypro1502@outlook.fr>
2021-01-03 14:02:45 +0100

    Add __getattr__ to Model class

--- models.py
+++ models.py
@@ -9,3 +9,12 @@ class Model:
 
     def __init__(self, data=None):
         self.data = DataWrapper() if data is None else data
+
+
+    def __getattr__(self, key):
+        if key == 'train_set':
+            return self.data.train_set
+        if key == 'test_set':
+            return self.data.test_set
+        if key == 'label':
+            return self.data.label
